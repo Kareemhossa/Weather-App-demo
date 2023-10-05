47d75c37b1786dcabb0ddde4e2d6247d5b86749b
Kareem Hossam <110343362+Kareemhossa@users.noreply.github.com>
2023-10-05 03:08:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
-// code deal with the api
-let weather = {
-    apiKey  : "2e094dcebed4e5cf1b716216e7aa9677" ,
-   fetchWeather : function (city){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+// Func to get API
+function fetchWeather(city) {
+  const apiKey = "2e094dcebed4e5cf1b716216e7aa9677";
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  )
     .then((response) => response.json())
-    .then((data) => this.displatWeather(data))
-    .catch(() => alert('No weather found'));; 
-   } ,
-
-
-   // code dispaly  weather with data
-   displatWeather : function (data){
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    // console.log(name,  icon, description , temp, humidity , speed) ; 
-    document.querySelector(".city").innerHTML ="Weather in " + name ;
-    document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
-    document.querySelector("#search-bar").value = "";
-   },
-
-   search: function () {
-    this.fetchWeather(document.querySelector("#search-bar").value);
-  },
+    .then((data) => displayWeather(data))
+    .catch((error) => alert("No weather found"));
 }
 
-// code work with search bar
-document.querySelector(".top-search button ").addEventListener("click", function () {
-    weather.search();
-  });
+function displayWeather(data) {
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const { temp, humidity } = data.main;
+  const { speed } = data.wind;
 
-//  code work with search bar when press ENTER
-  document.querySelector("#search-bar").addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
+  document.querySelector(".city").innerHTML = `Weather in ${name}`;
+  document.querySelector(".temp").innerText = `${temp}°C`;
+  document.querySelector(
+    ".icon"
+  ).src = `https://openweathermap.org/img/wn/${icon}.png`;
+  document.querySelector(".description").innerText = description;
+  document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+  document.querySelector(".wind").innerText = `Wind speed: ${speed} km/h`;
+  document.querySelector(".weather").classList.remove("loading");
+  document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`;
+  document.querySelector("#search-bar").value = "";
+}
 
-weather.fetchWeather("Cairo");
+//func search city
+function search() {
+  const city = document.querySelector("#search-bar").value;
+  if (city) {
+    fetchWeather(city);
+  } else {
+    alert("Please enter a city name");
+  }
+}
+
+//search bar event
+document
+  .querySelector(".top-search button ")
+  .addEventListener("click", search());
+
+document.querySelector("#search-bar").addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    search();
+  }
+});
+
+// Initial weather for Cairo
+fetchWeather("Cairo");
